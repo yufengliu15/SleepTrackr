dde497b69cf68032b5af0c9180aefcd53b63397f
Yufeng <92955983+yufengliu15@users.noreply.github.com>
2023-03-31 10:51:34 -0400

    Add files via upload

---
+++ SleepTrackr/SleepTrackr/SleepTrackrApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepTrackrApp.swift
+//  SleepTrackr
+//
+//  Created by Yufeng Liu on 2023-03-31.
+//
+
+import SwiftUI
+
+@main
+struct SleepTrackrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SleepTrackr/SleepTrackrUITests/SleepTrackrUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SleepTrackrUITestsLaunchTests.swift
+//  SleepTrackrUITests
+//
+//  Created by Yufeng Liu on 2023-03-31.
+//
+
+import XCTest
+
+final class SleepTrackrUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
